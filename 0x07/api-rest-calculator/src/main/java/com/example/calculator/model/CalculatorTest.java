293519b61117c48139d75d5ebb41221ac3d60443
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-02-21 18:08:19 -0300

    Update CalculatorTest.java

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/CalculatorTest.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/CalculatorTest.java
@@ -1,47 +1,43 @@
+package com.example.calculator.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
     @Test
-    void sumTest() {
-        // TODO
+    public void testAddNumbers() {
+        assertEquals(5, Calculator.add(2, 3));
     }
 
     @Test
-    public void numbersNullSumTest() {
-            // TODO
+    public void testSubNumbers() {
+        assertEquals(1, Calculator.subtract(3, 2));
     }
 
     @Test
-    void subTest() {
-       // TODO
+    public void testDivideNumbers() {
+        assertEquals(2.0, Calculator.divide(6, 3));
     }
 
     @Test
-    void divideTest() {
-       // TODO
+    public void testFactorial() {
+        assertEquals(120, Calculator.factorial(5));
     }
 
     @Test
-    public void divisionByZeroTest() {
-        // TODO
+    public void testCalculeDayBetweenDate() {
+        assertEquals(5, Calculator.calculeDayBetweenDate("2024-02-01", "2024-02-06"));
     }
 
     @Test
-    void factorialTest() {
-            // TODO
+    public void testIntegerToBinary() {
+        assertEquals("1010", Calculator.integerToBinary(10));
     }
 
     @Test
-    void integerToBinaryTest() {
-       // TODO
-    }
-
-    @Test
-    void integerToHexadecimalTest() {
-       // TODO
-    }
-
-    @Test
-    void calculeDayBetweenDateTest() {
-       // TODO
+    public void testIntegerToHexadecimal() {
+        assertEquals("a", Calculator.integerToHexadecimal(10));
     }
 }
