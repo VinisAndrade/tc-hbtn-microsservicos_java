5da10cf98687fdce97453a493bb5604c56f4bdc6
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-02-21 18:10:11 -0300

    Update CalculatorTest.java

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/CalculatorTest.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/model/CalculatorTest.java
@@ -1,41 +1,31 @@
 package com.example.calculator.model;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 public class CalculatorTest {
-
     @Test
     public void testAddNumbers() {
         assertEquals(5, Calculator.add(2, 3));
     }
-
     @Test
     public void testSubNumbers() {
         assertEquals(1, Calculator.subtract(3, 2));
     }
-
     @Test
     public void testDivideNumbers() {
         assertEquals(2.0, Calculator.divide(6, 3));
     }
-
     @Test
     public void testFactorial() {
         assertEquals(120, Calculator.factorial(5));
     }
-
     @Test
     public void testCalculeDayBetweenDate() {
         assertEquals(5, Calculator.calculeDayBetweenDate("2024-02-01", "2024-02-06"));
     }
-
     @Test
     public void testIntegerToBinary() {
         assertEquals("1010", Calculator.integerToBinary(10));
     }
-
     @Test
     public void testIntegerToHexadecimal() {
         assertEquals("a", Calculator.integerToHexadecimal(10));
